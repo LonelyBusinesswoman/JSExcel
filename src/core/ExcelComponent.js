e829e5d6b77db0743fb3d4ffa574ed375e2da1c5
Lizaveta <elis.liza567@gmail.com>
2020-10-12 19:20:39 +0300

    delete eventListeners

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,8 +1,21 @@
-import { DomListener } from "./DomListener";
+import {DomListener} from '@core/DomListener'
 
 export class ExcelComponent extends DomListener {
-    // Return template of component
-    toHTML(){
-        return ''
-    }
+  constructor($root, options = {}) {
+    super($root, options.listeners)
+    this.name = options.name || ''
+  }
+
+  // Возвращает шаблон компонента
+  toHTML() {
+    return ''
+  }
+
+  init() {
+    this.initDOMListeners()
+  }
+
+  destroy() {
+    this.removeDOMListeners()
+  }
 }
